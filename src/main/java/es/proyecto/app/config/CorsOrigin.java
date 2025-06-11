721b44399679b71ac23655511b52340896231422
JCRodriguez93 <jorge_madro@hotmail.com>
2025-06-12 00:45:46 +0200

    it should work now

---
+++ src/main/java/es/proyecto/app/config/CorsOrigin.java
@@ -0,0 +1,23 @@
+package es.proyecto.app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsOrigin {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://proyectodaw-3bt8.onrender.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
